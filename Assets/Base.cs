fe5c41400f40ab2697f8fe11111267831f8457fb
koybz <c15523157@mydit.ie>
2019-03-26 10:42:35 +0000

    Update Base.cs

--- Assets/Base.cs
+++ Assets/Base.cs
@@ -15,15 +15,34 @@ public class Base : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //calling spawn function for fighter
+        InvokeRepeating ("Spawn");
+
         foreach (Renderer r in GetComponentsInChildren<Renderer>())
         {
             r.material.color = Color.HSVToRGB(Random.Range(0.0f, 1.0f), 1, 1);
         }
+
+
+    }
+
+    void Spawn()
+    {
+
+        if(tiberium == 10)
+        {
+            Instantiate (fighterPrefab);
+            tiberium = tiberium - 10;
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         text.text = "" + tiberium;
+        tiberium += Time.deltaTime;
+
     }
 }
